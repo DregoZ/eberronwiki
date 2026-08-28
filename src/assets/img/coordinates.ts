3bbdf0c835af0f9b1da9c762e8ddeadc5dc33371
DregoZ <xanton.ac@gmail.com>
2026-08-28 14:00:55 +0200

    feat: add Darguun and Rhukaan Draal lore pages along with artefact documentation

--- src/assets/img/coordinates.ts
+++ src/assets/img/coordinates.ts
@@ -1,9 +1,10 @@
 /*
 const img = document.querySelector(
-  'img[src="/assets/content/eberron/mas-alla/mapamundi.jpg"]',
+  'img[src="/assets/content/eberron/khorvaire/darguun/darguun.png"]',
 );
-img.addEventListener('click', (e) => {
-  const rect = img.getBoundingClientRect();
+img?.addEventListener('pointerdown', (e: PointerEvent) => {
+  const target = e.target as HTMLImageElement;
+  const rect = target.getBoundingClientRect();
   const x = ((e.clientX - rect.left) / rect.width) * 100;
   const y = ((e.clientY - rect.top) / rect.height) * 100;
   console.log({ x: x.toFixed(2), y: y.toFixed(2) });
